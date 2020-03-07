d2c6263821197c44259a487b85ff7b4a25715a6b
Lisa Jung <lisahjung@gmail.com>
2020-03-07 02:05:55 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,11 +19,9 @@ def bonus
       ]
   }
   }
- end 
 
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
